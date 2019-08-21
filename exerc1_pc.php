73673115711d654f0c3ba67ad4e181d7ba2c54b0
*VogtLe 威廉* <48601875+VogtLeJato@users.noreply.github.com>
2019-08-20 23:38:53 -0300

    Update exerc1_pc.php

--- exerc1_pc.php
+++ exerc1_pc.php
@@ -1,29 +1,29 @@
-<! DOCTYPE html>
+<!DOCTYPE html>
 
-<html> <cabeça>
-<meta charset = "utf-8" />
+<html> <head>
+<meta charset="utf-8" />
 
-<title> Exibe os feedbacks </ title>
+<title>Exibe os feedbacks</title>
 
-</ head> <body>
-<h1> Feedback do Usuário </ h1>
+</head> <body>
+<h1>Feedback do Usuário</h1>
 
-<? php
-// Detalhes da conexão do banco de dados para mySQL
-$ host = 'localhost'; $ user = 'aluno'; $ passw = 'aluno'; $ dbname = '';
+<?php
+//Database connection details to mySQL
+$host = 'localhost'; $user = 'aluno'; $passw = 'aluno'; $dbname = '';
 
 
 
-// Faça uma conexão com o banco de dados
-$ conn = mysqli_connect ($ host, $ user, $ passw, $ dbname) ou morre
-("Incapaz de conectar!");
-// Cria a consulta SQL
-$ query = "SELECT user_id, e-mail, nome, escola, função de membros ORDER BY email ASC";
-$ result = mysqli_query ($ conn, $ query) ou die ("Erro na consulta");
+//Make a connection to the database
+$conn = mysqli_connect($host, $user, $passw, $dbname) or die
+("Unable to connect!");
+//Create the SQL query
+$query = "SELECT user_id, email, nome, escola, função, FROM membros ORDER BY email ASC";
+$result = mysqli_query( $conn, $query ) or die ("Error in query");
 
-// Buscar o resultado em um array associativo
-while ($ row = mysqli_fetch_assoc ($ result)) {
-$ table [] = $ linha; // adiciona cada linha ao array da tabela
+//Fetch the result into an associative array
+while ( $row = mysqli_fetch_assoc( $result ) ) {
+$table[] = $row; //add each row into the table array
 }
 
 ?>
@@ -32,53 +32,53 @@ $ table [] = $ linha; // adiciona cada linha ao array da tabela
 
 <table>
 <tr>
-<td> <strong> Membro </ strong> </ td>
-<td width = "10"> & nbsp; </ td>
-<td> <strong> E-mail </ strong> </ td>
-<td width = "10"> & nbsp; </ td>
-<td> <strong> Escola </ strong> </ td>
-<td width = "10"> & nbsp; </ td>
-<td> <strong> Estado </ strong> </ td>
-<td width = "10"> & nbsp; </ td>
-<td> <strong> Função </ strong> </ td>
-<td width = "10"> & nbsp; </ td>
-<td> <strong> Numero da equipe </ strong> </ td>
-</ tr>
-<? php
+<td><strong>Membro</strong></td>
+<td width="10">&nbsp;</td>
+<td><strong>Email</strong></td>
+<td width="10">&nbsp;</td>
+<td><strong>Escola</strong></td>
+<td width="10">&nbsp;</td>
+<td><strong>Estado</strong></td>
+<td width="10">&nbsp;</td>
+<td><strong>Função</strong></td>
+<td width="10">&nbsp;</td>
+<td><strong>Numero da equipe</strong></td>
+</tr>
+<?php
 
-if ($ table) {// Verifique se existem linhas a serem exibidas
-// Recupera cada elemento do array
-foreach ($ table as $ d_row) {
+if ($table) { //Check if there are any rows to be displayed
+//Retrieve each element of the array
+foreach($table as $d_row) {
 
 ?>
 
 <tr>
-<td> <? php echo ($ d_row ["nome"]); ?> </ td>
-<td width = "10"> & nbsp; </ td>
-<td> <? php echo ($ d_row ["email"]); ?> </ td>
-<td width = "10"> & nbsp; </ td>
-<td> <? php echo ($ d_row ["escola"]); ?> </ td>
-<td width = "10"> & nbsp; </ td>
-<td> <? php echo ($ d_row ["função"]); ?> </ td>
-<td width = "10"> & nbsp; </ td>
-<td> <? php echo ($ d_row ["número da equipe"]); ?> </ td>
+<td><?php echo($d_row["nome"]); ?></td>
+<td width="10">&nbsp;</td>
+<td><?php echo($d_row["email"]); ?></td>
+<td width="10">&nbsp;</td>
+<td><?php echo($d_row["escola"]); ?></td>
+<td width="10">&nbsp;</td>
+<td><?php echo($d_row["função"]); ?></td>
+<td width="10">&nbsp;</td>
+<td><?php echo($d_row["numero da equipe"]); ?></td>
 
-</ tr>
+</tr>
 
-<? php
+<?php
 
 }
 }
 
 ?>
-</ table>
+</table>
 
-<p> Número de Registros: <? php echo (mysqli_num_rows ($ result)); ?> </ p>
+<p>Número de Registros : <?php echo(mysqli_num_rows($result)); ?></p>
 
-<? php
+<?php
 
-mysqli_close ($ conn);
+mysqli_close($conn);
 
 ?>
-</ body>
-</ html>
+</body>
+</html>
